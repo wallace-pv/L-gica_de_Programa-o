798577df3706f113816a7c11a78c70c9edbbf30b
wallace-pv <wallace-pv>
2024-12-08 14:18:46 -0300

    Corrigindo alugames com a resolucão da alura

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -3,21 +3,17 @@ function alterarStatus(id){
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-    let texto= gameClicado.querySelector('a');
+    //let texto= gameClicado.querySelector('a');
     console.log(nomeJogo.textContent);
     if(imagem.classList.contains('dashboard__item__img--rented')){
         imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        //texto.innerHTML = ('Alugar');
+        botao.textContent = 'Alugar';
     }else { 
         imagem.classList.add('dashboard__item__img--rented');
-    }
-    if(botao.classList.contains('dashboard__item__button--return')){
-        botao.classList.remove('dashboard__item__button--return');
-        texto.innerHTML = ('Alugar');
-
-    }else{
         botao.classList.add('dashboard__item__button--return');
-        texto.innerHTML = ('Devolver');
+        //texto.innerHTML = ('Devolver');
+        botao.textContent = 'Devolver';
     }
-        
-    
 }
