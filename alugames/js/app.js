ba1ea219e161c9549773a70c4a40dadb1a6aaf3b
wallace-pv <wallace-pv>
2024-12-08 17:18:07 -0300

    Adicionado uma solucao com operador ternario

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,10 +1,12 @@
 function alterarStatus(id){
+    
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     //let texto= gameClicado.querySelector('a');
     console.log(nomeJogo.textContent);
+    
     if(imagem.classList.contains('dashboard__item__img--rented')){
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
@@ -16,4 +18,8 @@ function alterarStatus(id){
         //texto.innerHTML = ('Devolver');
         botao.textContent = 'Devolver';
     }
+//solução com operador Ternario
+//imagem.classList.contains('dashboard__item__img--rented') ? (imagem.classList.remove('dashboard__item__img--rented'), botao.classList.remove('dashboard__item__button--return'), botao.textContent = 'Alugar') : (imagem.classList.add('dashboard__item__img--rented'), botao.classList.add('dashboard__item__button--return'), botao.textContent = 'Devolver');
+
 }
+
